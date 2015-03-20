0edd6fdbdfda2bbb27d672ae807c2e5b182b12c9
montselozanod <montse_967@hotmail.com>
2015-03-20 18:08:12 -0500

    added facebook to build gradle

--- app/src/main/java/mindpop/learnpop/MainActivity.java
+++ app/src/main/java/mindpop/learnpop/MainActivity.java
@@ -17,7 +17,7 @@ import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
-
+import com.facebook.Session;
 
 public class MainActivity extends ActionBarActivity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
