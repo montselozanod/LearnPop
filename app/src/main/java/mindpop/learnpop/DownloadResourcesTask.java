8ddd138a27089d6c0abe0508eea9c5804fcb53af
montselozanod <montse_967@hotmail.com>
2015-03-27 04:07:41 -0500

    cambios a xml

---
+++ app/src/main/java/mindpop/learnpop/DownloadResourcesTask.java
@@ -0,0 +1,16 @@
+package mindpop.learnpop;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by montselozanod on 3/27/15.
+ */
+// extends AsyncTask<String, Void, String>
+public class DownloadResourcesTask{
+
+    protected String doInBackground(){
+        String url="";
+
+        return url;
+    }
+}
