9480327a77b3cce5ea6c4120a0eb8bdcc4869186
montselozanod <montse_967@hotmail.com>
2015-03-29 13:03:56 -0500

    changes to json parser to check for empty query

--- app/src/main/java/mindpop/learnpop/Articles.java
+++ app/src/main/java/mindpop/learnpop/Articles.java
@@ -1,6 +1,7 @@
 package mindpop.learnpop;
 
 import android.app.ProgressDialog;
+import android.content.SharedPreferences;
 import android.util.Log;
 import android.os.Bundle;
 import android.os.AsyncTask;
@@ -24,81 +25,42 @@ import org.json.JSONObject;
  */
 public class Articles extends ListFragment {
 
+    public static final String PREFS_NAME = "UserPrefs";
     private ProgressDialog pDialog;
-
+    private Learnpop learnApp;
+    private LoadResource loadResource;
     JSONParser jsonParser = new JSONParser();
     ArrayList<Resource> resourceArrayList;
-
-    private static String urlResources = "http://portfolioartstudio.com/php/getResourceHtmlTable.php";
-    private final String TAG_SUCCESS = "success";
-    private final String TAG_Type = "Type";
-    private final String Tag_Subject = "Subject";
-    private final String Tag_Grade = "GradeLevel";
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_article, container, false);
-
+        //restore prefs
+        restorePreferences();
+        //loadResource = new LoadResource(getActivity(),  ,);
         resourceArrayList = new ArrayList<Resource>();
-        new LoadResources().execute();
 
 
+        //learnApp = ((Learnpop)getApplicationContext());
         return rootView;
 
     }
 
-    class LoadResources extends AsyncTask<String, String, String>{
-        @Override
-        protected void onPreExecute(){
-            super.onPreExecute();
-            pDialog = new ProgressDialog(getActivity());
-            pDialog.setMessage("Loading resources...");
-            pDialog.setIndeterminate(false);
-            pDialog.setCancelable(false);
-            pDialog.show();
-        }
+    private void restorePreferences(){
+        SharedPreferences settings = this.getActivity().getSharedPreferences(PREFS_NAME, 0);
+        boolean visual = settings.getBoolean("visual", false);
+        boolean drama = settings.getBoolean("drama", false);
+        boolean socials = settings.getBoolean("socials", false);
+        boolean science = settings.getBoolean("science", false);
+        boolean math = settings.getBoolean("math", false);
+        boolean movement = settings.getBoolean("movement", false);
+        boolean digital = settings.getBoolean("digital", false);
+        boolean bilingual = settings.getBoolean("bilingual", false);
+        boolean ela = settings.getBoolean("ela", false);
+        boolean music = settings.getBoolean("music", false);
 
-        protected String doInBackground(String... args){
-            String result = "";
-            //parameters
-            ArrayList<NameValuePair> params = new ArrayList<NameValuePair>();
-            JSONObject json = jsonParser.makeHttpRequest(urlResources, "GET", params);
-
-            Log.d("Resources: ", json.toString());
-
-            try{
-                int success = json.getInt(TAG_SUCCESS);
-                if(success == 1){
-                    //resourceArrayList = json.getJSONArray();
-                }
-            }catch(JSONException e){
-                e.printStackTrace();
-            }
-
-            return result;
-        }
-
-        protected  void onPostExecute(String result){
-            try{
-                JSONArray jArray = new JSONArray(result);
-                for(int i=0;i<jArray.length();i++){
-                    JSONObject json_data = jArray.getJSONObject(i);
-                    Resource rsc = new Resource();
-                    rsc.setTitle(json_data.getString("ResName"));
-                    rsc.setSubject(json_data.getString("Subject"));
-                    rsc.setGradelevel(json_data.getString("GradeLevel"));
-                    rsc.setUpVote(json_data.getInt("Likes"));
-                    resourceArrayList.add(i, rsc);
-                }
-            }catch(JSONException e){
-                e.printStackTrace();
-                Log.e("log_tag","Error parsing data "+ e.toString());
-            }
-        }
+        String gradelevel = settings.getString("grade", "LowerElem");
     }
-
-
 
 }
---
+++ app/src/main/java/mindpop/learnpop/LoadResource.java
@@ -0,0 +1,113 @@
+package mindpop.learnpop;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.content.Context;
+import android.widget.ListAdapter;
+import android.widget.SimpleAdapter;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * Created by montselozanod on 3/27/15.
+ */
+public class LoadResource extends AsyncTask<String, String, String> {
+
+    private Context _context;
+    private int displayType; // 0 for articles and 1 for videos
+    private ProgressDialog pDialog;
+    JSONParser jsonParser = new JSONParser();
+    private ArrayList<Resource> resourcesArrayList;
+    private static String urlResources = "http://austinartmap.com/CreativeTeach/PHP/getResourcesList.php";
+    private String [] subjects;
+    private String grade;
+    private JSONArray resources;
+    private final String TAG_SUCCESS = "success";
+
+    public LoadResource(Context context, String [] subjects, String grade, int type){
+        this._context = context;
+        this.subjects = subjects;
+        this.grade = grade;
+        this.displayType = type;
+    }
+    @Override
+    protected void onPreExecute(){
+        super.onPreExecute();
+        pDialog = new ProgressDialog(_context);
+        pDialog.setMessage("Loading resources...");
+        pDialog.setIndeterminate(false);
+        pDialog.setCancelable(false);
+        pDialog.show();
+    }
+
+    protected String doInBackground(String... args){
+
+        //parameters
+        ArrayList<NameValuePair> params = new ArrayList<NameValuePair>();
+
+            params.add(new BasicNameValuePair("GradeLevel[]", grade));
+       
+
+        for(int i = 0; i < subjects.length; i++){
+            params.add(new BasicNameValuePair("Subject[]", subjects[i]));
+        }
+        JSONObject json = jsonParser.makeHttpRequest(urlResources, "GET", params);
+        Log.d("Resources: ", json.toString());
+
+        try{
+            int success = json.getInt(TAG_SUCCESS);
+
+            if(success == 1){
+                for (int i = 0; i < resources.length(); i++){
+                    JSONObject c = resources.getJSONObject(i);
+
+                    Resource res = new Resource();
+                    res.setTitle(c.getString("ResName"));
+                    res.setResourceId(c.getInt("ResID"));
+                    res.setSubject(c.getString("Subject"));
+                    res.setType(c.getString("ResType"));
+                    res.setUpVote(c.getInt("Likes"));
+                    res.setDownVote(c.getInt("Dislikes"));
+                    //date format
+                    //2014-02-28
+                    String dateString =c.getString("PublishingDate");
+
+                    try{
+                        SimpleDateFormat formatter =
+                            new SimpleDateFormat("yyyy-MM-dd");
+                        Date date = formatter.parse(dateString);
+                        res.setPublishDate(date); //change to date
+                    }catch(ParseException e){
+                        e.printStackTrace();
+                    }
+                    res.setUrl(c.getString("ResURL"));
+                    res.setSummary(c.getString("Summary"));
+                }
+            }else{
+                //if there are no resources
+
+            }
+
+        }catch(JSONException e){
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    protected  void onPostExecute(JSONObject result)
+    {
+        pDialog.dismiss();
+    }
+
+}
