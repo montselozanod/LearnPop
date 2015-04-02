56f71d340eab87f5b28cf9050d0701707a762712
montselozanod <montse_967@hotmail.com>
2015-04-02 10:17:04 -0500

    change tinder card functionality to art partner

--- app/src/main/java/mindpop/learnpop/ArtPartnerFragment.java
+++ app/src/main/java/mindpop/learnpop/ArtPartnerFragment.java
@@ -1,11 +1,16 @@
 package mindpop.learnpop;
 
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.andtinder.model.CardModel;
+import com.andtinder.view.CardContainer;
+import com.andtinder.view.SimpleCardStackAdapter;
 
 
 /**
@@ -13,6 +18,7 @@ import android.view.ViewGroup;
  */
 public class ArtPartnerFragment extends Fragment {
 
+    private CardContainer mCardContainer;
 
     public ArtPartnerFragment() {
         // Required empty public constructor
@@ -23,7 +29,19 @@ public class ArtPartnerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_art_partner, container, false);
+        View rootView =  inflater.inflate(R.layout.fragment_art_partner, container, false);
+
+        mCardContainer = (CardContainer) rootView.findViewById(R.id.cardView);
+        Resources r = getResources();
+
+        SimpleCardStackAdapter adapter = new SimpleCardStackAdapter(getActivity());
+
+        adapter.add(new CardModel("Title1", "Description goes here", r.getDrawable(R.drawable.picture1)));
+        adapter.add(new CardModel("Title2", "Description goes here", r.getDrawable(R.drawable.picture2)));
+        adapter.add(new CardModel("Title3", "Description goes here", r.getDrawable(R.drawable.picture3)));
+        mCardContainer.setAdapter(adapter);
+
+        return rootView;
     }
 
 
--- app/src/main/java/mindpop/learnpop/Videos.java
+++ app/src/main/java/mindpop/learnpop/Videos.java
@@ -1,36 +1,23 @@
 package mindpop.learnpop;
 
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.andtinder.model.CardModel;
-import com.andtinder.view.CardContainer;
-import com.andtinder.view.SimpleCardStackAdapter;
 
 /**
  * Created by montselozanod on 3/20/15.
  */
 public class Videos extends Fragment {
 
-    private CardContainer mCardContainer;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_videos, container, false);
 
-        mCardContainer = (CardContainer) rootView.findViewById(R.id.cardView);
-        Resources r = getResources();
 
-        SimpleCardStackAdapter adapter = new SimpleCardStackAdapter(getActivity());
-
-        adapter.add(new CardModel("Title1", "Description goes here", r.getDrawable(R.drawable.picture1)));
-        adapter.add(new CardModel("Title2", "Description goes here", r.getDrawable(R.drawable.picture2)));
-        adapter.add(new CardModel("Title3", "Description goes here", r.getDrawable(R.drawable.picture3)));
-        mCardContainer.setAdapter(adapter);
         return rootView;
     }
 }
