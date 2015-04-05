c9ea8e41fa6316de92dfb94c9cddfe33ae203bf7
montselozanod <montse_967@hotmail.com>
2015-04-05 01:34:47 -0500

    add recycler adapter for resources

---
+++ app/src/main/java/mindpop/learnpop/ResourceAdapter.java
@@ -0,0 +1,61 @@
+package mindpop.learnpop;
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+/**
+ * Created by montselozanod on 4/5/15.
+ */
+public class ResourceAdapter extends RecyclerView.Adapter<RecyclerAdapter.ResourceViewHolder> {
+
+    List<Resource> resources;
+
+    ResourceAdapter(List<Resource> list){
+        this.resources = list;
+    }
+
+    public int getItemCount() {
+        return resources.size();
+    }
+
+    @Override
+    public ResourceViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.card_view_announce, viewGroup, false);
+        ResourceViewHolder pvh = new ResourceViewHolder(v);
+        return pvh;
+    }
+
+    @Override
+    public void onBindViewHolder(ResourceViewHolder aViewHolder, int i) {
+        aViewHolder.title.setText(resources.get(i).getTitle());
+        aViewHolder.subject.setText(resources.get(i).getSubject());
+        aViewHolder.icon.setImageResource(R.drawable.ic_like);
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+    public static class ResourceViewHolder extends RecyclerView.ViewHolder{
+        CardView res;
+        TextView title;
+        TextView subject;
+        ImageView icon;
+
+        ResourceViewHolder(View v){
+            super(itemView);
+
+        }
+
+    }
+}
