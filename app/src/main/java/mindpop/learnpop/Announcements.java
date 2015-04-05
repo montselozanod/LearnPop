a704aef6d8858040eef65e846b5fd4fca878396b
montselozanod <montse_967@hotmail.com>
2015-04-04 22:58:31 -0500

    xml card_view announcements

--- app/src/main/java/mindpop/learnpop/Announcements.java
+++ app/src/main/java/mindpop/learnpop/Announcements.java
@@ -2,6 +2,8 @@ package mindpop.learnpop;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,10 +13,23 @@ import android.view.ViewGroup;
  */
 public class Announcements extends Fragment {
 
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_announce, container, false);
+
+        mRecyclerView = (RecyclerView)rootView.findViewById(R.id.my_recycler_view);
+
+        // for performance given that changes in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        
+
         return rootView;
     }
 }
