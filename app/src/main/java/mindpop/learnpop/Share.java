d60b6e5e75aec6ca418319179cdf8d41895f42fb
montselozanod <montse_967@hotmail.com>
2015-04-08 21:09:03 -0500

    get layout items

--- app/src/main/java/mindpop/learnpop/Share.java
+++ app/src/main/java/mindpop/learnpop/Share.java
@@ -8,34 +8,48 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by montselozanod on 3/20/15.
  */
 public class Share extends Fragment {
-    ProgressDialog pDialog;
+    private ProgressDialog pDialog;
+    private TextView name;
+    private TextView resUrl;
+    private Spinner spinnerGrades;
+    private Spinner spinnerSubjects;
+    private Spinner spinnerType;
+    private Button btnSend;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_share, container, false);
         setSpinnerContent(rootView);
+        name = (TextView)rootView.findViewById(R.id.resName);
+        resUrl = (TextView)rootView.findViewById(R.id.editURL);
+        btnSend = (Button)rootView.findViewById(R.id.btnSend);
         return rootView;
     }
 
     private void setSpinnerContent(View view){
 
-        Spinner spinnerGrades = (Spinner) view.findViewById(R.id.spinner_Grades);
+        spinnerGrades = (Spinner) view.findViewById(R.id.spinner_Grades);
         ArrayAdapter <CharSequence> adapterG = ArrayAdapter.createFromResource(getActivity(), R.array.grades_array, android.R.layout.simple_spinner_item);
         adapterG.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerGrades.setAdapter(adapterG);
 
-        Spinner spinnerSubjects = (Spinner) view.findViewById(R.id.spinner_Subjects);
+        spinnerSubjects = (Spinner) view.findViewById(R.id.spinner_Subjects);
         ArrayAdapter <CharSequence> adapterS = ArrayAdapter.createFromResource(getActivity(), R.array.subjects_array, android.R.layout.simple_spinner_item);
         adapterS.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerSubjects.setAdapter(adapterS);
 
-        Spinner spinnerType = (Spinner) view.findViewById(R.id.spinner_Type);
+        spinnerType = (Spinner) view.findViewById(R.id.spinner_Type);
         ArrayAdapter <CharSequence> adapterT = ArrayAdapter.createFromResource(getActivity(), R.array.type_array, android.R.layout.simple_spinner_item);
         adapterT.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerType.setAdapter(adapterT);
@@ -53,7 +67,14 @@ public class Share extends Fragment {
         }
 
         protected String doInBackground(String... args){
-            String resName =
+            String resName = name.getText().toString();
+            String url = resUrl.getText().toString();
+            String type = spinnerType.getSelectedItem().toString();
+            String subject = spinnerSubjects.getSelectedItem().toString();
+            String grade = spinnerGrades.getSelectedItem().toString();
+
+            
+
         }
     }
 }
