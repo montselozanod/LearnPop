1a726da2853f87e25ece4adf43901e8ebb43d309
montselozanod <montse_967@hotmail.com>
2015-04-09 03:03:16 -0500

    progress bar for loading of page

--- app/src/main/java/mindpop/learnpop/WebViewFragment.java
+++ app/src/main/java/mindpop/learnpop/WebViewFragment.java
@@ -1,5 +1,6 @@
 package mindpop.learnpop;
 
+import android.graphics.Bitmap;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,12 +11,14 @@ import android.view.LayoutInflater;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.util.Log;
+import android.widget.ProgressBar;
 
 /**
  * Created by montselozanod on 3/29/15.
  */
 public class WebViewFragment extends Fragment {
     private String resourceURL;
+    private ProgressBar progress;
 
     public void init(String url){
         this.resourceURL = url;
@@ -41,7 +44,8 @@ public class WebViewFragment extends Fragment {
         Log.d("SwA", "WVF onCreateView");
         View v = inflater.inflate(R.layout.web_layout, container, false);
         setHasOptionsMenu(true);
-
+        progress = (ProgressBar) v.findViewById(R.id.progressBar);
+        progress.setMax(100);
         if (resourceURL != null) {
             Log.d("SwA", "Current URL  1["+resourceURL+"]");
 
@@ -49,7 +53,6 @@ public class WebViewFragment extends Fragment {
             wv.getSettings().setJavaScriptEnabled(true);
             wv.setWebViewClient(new SwAWebClient());
             wv.loadUrl(resourceURL);
-
         }
         return v;
     }
@@ -64,11 +67,30 @@ public class WebViewFragment extends Fragment {
 
     }
 
+    public void setValue(int progress) {
+        this.progress.setProgress(progress);
+    }
+
     private class SwAWebClient extends WebViewClient {
 
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
             return false;
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            progress.setVisibility(View.GONE);
+            progress.setProgress(100);
+            super.onPageFinished(view, url);
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            progress.setVisibility(View.VISIBLE);
+            progress.setProgress(0);
+            super.onPageStarted(view, url, favicon);
         }
 
     }
