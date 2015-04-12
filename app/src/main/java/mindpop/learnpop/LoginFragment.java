ac6b8525e9fd3386e7ad75f6dceaa1ce28f76995
montselozanod <montse_967@hotmail.com>
2015-04-12 04:42:24 -0500

    usable login

---
+++ app/src/main/java/mindpop/learnpop/LoginFragment.java
@@ -0,0 +1,144 @@
+package mindpop.learnpop;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class LoginFragment extends Fragment {
+
+
+    private TextView mTextDetails;
+    private CallbackManager mCallbackManager;
+    private AccessTokenTracker mTokenTracker;
+    private ProfileTracker mProfileTracker;
+    private FacebookCallback<LoginResult> mFacebookCallback = new FacebookCallback<LoginResult>() {
+        @Override
+        public void onSuccess(LoginResult loginResult) {
+            Log.d("VIVZ", "onSuccess");
+            AccessToken accessToken = loginResult.getAccessToken();
+            Profile profile = Profile.getCurrentProfile();
+            Log.d("VIVZ", profile.getFirstName());
+            mTextDetails.setText(constructWelcomeMessage(profile));
+
+        }
+
+
+        @Override
+        public void onCancel() {
+            Log.d("VIVZ", "onCancel");
+        }
+
+        @Override
+        public void onError(FacebookException e) {
+            Log.d("VIVZ", "onError " + e);
+        }
+    };
+
+
+    public LoginFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mCallbackManager = CallbackManager.Factory.create();
+        setupTokenTracker();
+        setupProfileTracker();
+
+        mTokenTracker.startTracking();
+        mProfileTracker.startTracking();
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        setupTextDetails(view);
+        setupLoginButton(view);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Profile profile = Profile.getCurrentProfile();
+        mTextDetails.setText(constructWelcomeMessage(profile));
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mTokenTracker.stopTracking();
+        mProfileTracker.stopTracking();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void setupTextDetails(View view) {
+        mTextDetails = (TextView) view.findViewById(R.id.login_text);
+    }
+
+    private void setupTokenTracker() {
+        mTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+                Log.d("VIVZ", "" + currentAccessToken);
+            }
+        };
+    }
+
+    private void setupProfileTracker() {
+        mProfileTracker = new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(Profile oldProfile, Profile currentProfile) {
+                Log.d("VIVZ", "" + currentProfile);
+                mTextDetails.setText(constructWelcomeMessage(currentProfile));
+            }
+        };
+    }
+
+    private void setupLoginButton(View view) {
+        LoginButton mButtonLogin = (LoginButton) view.findViewById(R.id.login_button);
+        mButtonLogin.setFragment(this);
+        mButtonLogin.setReadPermissions("user_friends");
+        mButtonLogin.registerCallback(mCallbackManager, mFacebookCallback);
+    }
+
+    private String constructWelcomeMessage(Profile profile) {
+        StringBuffer stringBuffer = new StringBuffer();
+        if (profile != null) {
+            stringBuffer.append("Welcome " + profile.getName());
+        }
+        return stringBuffer.toString();
+    }
+}
