92c045f6f4c6f76c7115b64259bc2f2c437ce21b
montselozanod <montse_967@hotmail.com>
2015-04-12 01:16:04 -0500

    delete session management uses

--- app/src/main/java/mindpop/learnpop/SpashScreen.java
+++ app/src/main/java/mindpop/learnpop/SpashScreen.java
@@ -15,20 +15,13 @@ public class SpashScreen extends Activity{
 
     //screen timer
     private static int SPASH_TIME_OUT = 3000;
-    private SessionManagement session;
+    //private SessionManagement session;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_spash);
 
-        new Handler().postDelayed(new Runnable(){
-            @Override
-            public void run(){
-                session = new SessionManagement(getApplicationContext());
-                session.checkLogin();
-                finish();
-            }
-        }, SPASH_TIME_OUT);
+       
     }
 }
