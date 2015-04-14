8bfe0e2d6cb6e3e581b41ac92905ce64b6c56a42
montselozanod <montse_967@hotmail.com>
2015-04-14 00:31:35 -0500

    found bug fix for bilingual

--- app/src/main/java/mindpop/learnpop/Preferences.java
+++ app/src/main/java/mindpop/learnpop/Preferences.java
@@ -6,17 +6,25 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckedTextView;
 import android.widget.Spinner;
 
 /**
  * Created by montselozanod on 3/20/15.
  */
 public class Preferences extends Fragment {
+    private CheckedTextView bilingual_check;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_preferences, container, false);
         setSpinnerContent(rootView);
+        bilingual_check = (CheckedTextView)rootView.findViewById(R.id.check_bilingual);
+        bilingual_check.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                ((CheckedTextView) v).toggle();
+            }
+        });
 
         return rootView;
     }
