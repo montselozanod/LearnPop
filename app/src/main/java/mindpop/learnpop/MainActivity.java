3aa5a23e16420a920bad976f0fe3bc2ddfc49a4e
montselozanod <montse_967@hotmail.com>
2015-04-15 10:20:58 -0500

    delete action settings from main activity

--- app/src/main/java/mindpop/learnpop/MainActivity.java
+++ app/src/main/java/mindpop/learnpop/MainActivity.java
@@ -158,21 +158,6 @@ public class MainActivity extends ActionBarActivity
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
     public void onResume() {
         super.onResume();
         isResumed = true;
