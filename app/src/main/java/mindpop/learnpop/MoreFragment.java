494f0579c6db82dd0b7505fb7d8710aae7078606
montselozanod <montse_967@hotmail.com>
2015-04-18 14:27:09 -0500

    fix bug in more

--- app/src/main/java/mindpop/learnpop/MoreFragment.java
+++ app/src/main/java/mindpop/learnpop/MoreFragment.java
@@ -35,7 +35,7 @@ public class MoreFragment extends Fragment {
         mRecyclerView.setHasFixedSize(true);
         mLayoutManager = new LinearLayoutManager(getActivity());
         mRecyclerView.setLayoutManager(mLayoutManager);
-        loadResource = new LoadResource(getActivity(), getActivity(), mRecyclerView, sub ,"LowerElem", 2);
+        loadResource = new LoadResource(getActivity(), getActivity(), mRecyclerView, 2);
 
         loadResource.execute();
         return rootView;
