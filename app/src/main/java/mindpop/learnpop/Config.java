774bf215ac1ccaf4d1ee211afb6c13e6954f0019
montselozanod <montse_967@hotmail.com>
2015-04-18 00:27:09 -0500

    string for youtube

--- app/src/main/java/mindpop/learnpop/Config.java
+++ app/src/main/java/mindpop/learnpop/Config.java
@@ -4,4 +4,6 @@ package mindpop.learnpop;
  * Created by montselozanod on 4/18/15.
  */
 public class Config {
+
+    public static final String DEVELOPER_KEY = "AIzaSyC46ujLQA6xErMpIpHzNGfnfM9WfjQjaeQ";
 }
