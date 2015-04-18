1554a7fd9302a13127cf198cbb0260aacbaea2ce
montselozanod <montse_967@hotmail.com>
2015-04-18 13:01:27 -0500

    override hashcode

--- app/src/main/java/mindpop/learnpop/Resource.java
+++ app/src/main/java/mindpop/learnpop/Resource.java
@@ -135,6 +135,15 @@ public class Resource {
     }
 
     @Override
+    public int hashCode(){
+        final int prime = 31;
+        int result = 1;
+        result = prime * result
+                + ((url == null) ? 0 : url.hashCode());
+        return result;
+    }
+
+    @Override
     public boolean equals(Object obj){
         if(obj == this){
             return true;
