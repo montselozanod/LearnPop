01990045c596ca41762001d3f4f2640bee9971ff
montselozanod <montse_967@hotmail.com>
2015-04-20 03:45:16 -0500

    list partners

--- app/src/main/java/mindpop/learnpop/GridViewAdapter.java
+++ app/src/main/java/mindpop/learnpop/GridViewAdapter.java
@@ -1,71 +1,132 @@
 package mindpop.learnpop;
 
+import android.app.Activity;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.provider.Telephony;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
+import java.io.InputStream;
+import java.net.URL;
 import java.util.List;
 
 /**
- * Created by montselozanod on 4/20/15.
+ * Created by montselozanod on 4/5/15.
  */
-class GridViewAdapter extends BaseAdapter {
-    private Context mContext;
-    private List<Partner> mItems;
+public class PartnerAdapter extends RecyclerView.Adapter<PartnerAdapter.PartnerViewHolder> {
 
-    public GridViewAdapter(Context context, List<Partner> items) {
-        mContext = context;
-        mItems = items;
+    List<Partner> resources;
+    private FragmentActivity _activity;
+
+    PartnerAdapter(FragmentActivity ac, List<Partner> list){
+        this._activity= ac;
+        this.resources = list;
+    }
+
+    public int getItemCount() {
+        return resources.size();
     }
 
     @Override
-    public int getCount() {
-        return mItems.size();
+    public PartnerViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.card_view_strategy, viewGroup, false);
+        PartnerViewHolder pvh = new PartnerViewHolder(v);
+        return pvh;
     }
 
     @Override
-    public Object getItem(int position) {
-        return mItems.get(position);
+    public void onBindViewHolder(PartnerViewHolder aViewHolder, int i) {
+        aViewHolder.setClickListener(new PartnerViewHolder.ClickListener(){
+            @Override
+            public void onClick(View v, int pos, boolean isLongClick){
+                Partner item = (Partner)resources.get(pos);
+                /*WebViewFragment webFrag = new WebViewFragment();
+                webFrag.init(item.getUrl());*/
+                PartnerItem webItem = new PartnerItem();
+                webItem.init(item);
+                _activity.getSupportFragmentManager().beginTransaction().replace(R.id.container, webItem).addToBackStack(null).commit();
+            }
+        });
+
+        aViewHolder.title.setText(resources.get(i).getParName());
+        String sub = resources.get(i).getSubject();
+        aViewHolder.subject.setText(sub);
+        int resID = 0;
+        switch(sub){
+            case "Bilingual": resID = R.drawable.bilingual; break;
+            case "Digital Media": resID = R.drawable.media; break;
+            case "Drama": resID = R.drawable.drama; break;
+            case "ELA": resID = R.drawable.ela; break;
+            case "History": resID = R.drawable.history; break;
+            case "Math": resID = R.drawable.math; break;
+            case "Movement": resID = R.drawable.move; break;
+            case "Music": resID = R.drawable.music; break;
+            case "Science": resID = R.drawable.science; break;
+            case "SEL": resID = R.drawable.sel; break;
+            case "Visual Art": resID = R.drawable.art; break;
+        }
+        aViewHolder.icon.setImageResource(resources.get(i).getImage());
     }
 
     @Override
-    public long getItemId(int position) {
-        return position;
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
     }
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        ViewHolder viewHolder;
+    public static class PartnerViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener{
+        CardView res;
+        TextView title;
+        TextView subject;
+        ImageView icon;
 
-        if(convertView == null) {
-            // inflate the GridView item layout
-            LayoutInflater inflater = LayoutInflater.from(mContext);
-            convertView = inflater.inflate(R.layout.grid_view_item, parent, false);
+        private ClickListener clickListener;
 
-            // initialize the view holder
-            viewHolder = new ViewHolder();
-            viewHolder.ivIcon = (ImageView) convertView.findViewById(R.id.ivIcon);
-            viewHolder.tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
-            convertView.setTag(viewHolder);
-        } else {
-            // recycle the already inflated view
-            viewHolder = (ViewHolder) convertView.getTag();
+        public interface ClickListener{
+
+            public void onClick(View v, int position, boolean isLongClick);
         }
 
-        // update the item view
-        Partner item = (Partner) mItems.get(position);
-        viewHolder.ivIcon.setImageDrawable(item.getImage());
-        viewHolder.tvTitle.setText(item.getParName());
+        PartnerViewHolder(View itemView){
+            super(itemView);
+            res = (CardView)itemView.findViewById(R.id.card_resource);
+            title = (TextView)itemView.findViewById(R.id.res_title);
+            subject = (TextView)itemView.findViewById(R.id.res_sub);
+            icon = (ImageView)itemView.findViewById(R.id.icon_res);
+            itemView.setOnClickListener(this);
+            itemView.setOnLongClickListener(this);
+        }
 
-        return convertView;
+        public void setClickListener(ClickListener clickListener) {
+            this.clickListener = clickListener;
+        }
+
+        @Override
+        public void onClick(View v) {
+
+            // If not long clicked, pass last variable as false.
+            clickListener.onClick(v, getPosition(), false);
+        }
+
+        @Override
+        public boolean onLongClick(View v) {
+
+            // If long clicked, passed last variable as true.
+            clickListener.onClick(v, getPosition(), true);
+            return true;
+        }
     }
 
-    private static class ViewHolder {
-        ImageView ivIcon;
-        TextView tvTitle;
-    }
 }
+
