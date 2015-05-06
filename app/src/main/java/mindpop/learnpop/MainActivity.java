0b48201c9488f0ab57a367a84596d18976fcc2a5
montselozanod <montse_967@hotmail.com>
2015-05-06 01:26:42 -0500

    change add to replace

--- app/src/main/java/mindpop/learnpop/MainActivity.java
+++ app/src/main/java/mindpop/learnpop/MainActivity.java
@@ -88,7 +88,7 @@ public class MainActivity extends ActionBarActivity
                 fragment = new Announcements();
                 mTitle = stringArray[position];
                 fragmentManager.beginTransaction()
-                        .add(R.id.container, fragment)
+                        .replace(R.id.container, fragment)
                         .addToBackStack(null)
                         .commit();
                 break;
@@ -97,7 +97,7 @@ public class MainActivity extends ActionBarActivity
                 fragment = new Articles();
                 mTitle = stringArray[position];
                 fragmentManager.beginTransaction()
-                        .add(R.id.container, fragment)
+                        .replace(R.id.container, fragment)
                         .addToBackStack(null)
                         .commit();
                 break;
@@ -124,7 +124,7 @@ public class MainActivity extends ActionBarActivity
                 fragment = new Favorites();
                 mTitle = stringArray[position];
                 fragmentManager.beginTransaction()
-                        .add(R.id.container, fragment)
+                        .replace(R.id.container, fragment)
                         .addToBackStack(null)
                         .commit();
                 break;
@@ -133,7 +133,7 @@ public class MainActivity extends ActionBarActivity
                 fragment = new ArtPartnerFragment();
                 mTitle = stringArray[position];
                 fragmentManager.beginTransaction()
-                        .add(R.id.container, fragment)
+                        .replace(R.id.container, fragment)
                         .addToBackStack(null)
                         .commit();
                 break;
@@ -142,7 +142,7 @@ public class MainActivity extends ActionBarActivity
                 fragment = new Share();
                 mTitle = stringArray[position];
                 fragmentManager.beginTransaction()
-                        .add(R.id.container, fragment)
+                        .replace(R.id.container, fragment)
                         .addToBackStack(null)
                         .commit();
                 break;
@@ -151,7 +151,7 @@ public class MainActivity extends ActionBarActivity
                 fragment = new Preferences();
                 mTitle = stringArray[position];
                 fragmentManager.beginTransaction()
-                        .add(R.id.container, fragment)
+                        .replace(R.id.container, fragment)
                         .addToBackStack(null)
                         .commit();
                 break;
@@ -160,7 +160,7 @@ public class MainActivity extends ActionBarActivity
                 fragment = new HelpFragment();
                 mTitle = stringArray[position];
                 fragmentManager.beginTransaction()
-                        .add(R.id.container, fragment)
+                        .replace(R.id.container, fragment)
                         .addToBackStack(null)
                         .commit();
                 break;
